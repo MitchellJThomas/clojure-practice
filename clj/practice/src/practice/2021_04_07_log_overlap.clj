484ce4bd80482041249b510fef43b231c010757c
Mitch Thomas <mthomas@tripwire.com>
2021-04-20 23:16:36 -0700

    Getting closer with the overlap counts

--- clj/practice/src/practice/2021_04_07_log_overlap.clj
+++ clj/practice/src/practice/2021_04_07_log_overlap.clj
@@ -9,7 +9,52 @@
                       end-time-shift #(jt/minutes (rand-int 120))]
                   (map (fn [s] {:start s :end (jt/plus s (end-time-shift))}) start-times))))
 
+(def test-data-zero-overlaps
+  [{:start (jt/local-date-time 2021 04 20 20 00 00)
+    :end   (jt/local-date-time 2021 04 20 20 10 01)}
+   {:start (jt/local-date-time 2021 04 20 20 10 02)
+    :end   (jt/local-date-time 2021 04 20 20 20 01)}
+   {:start (jt/local-date-time 2021 04 20 20 20 02)
+    :end   (jt/local-date-time 2021 04 20 20 30 01)}
+   ])
+
+(def test-data-two-overlaps-1
+  [{:start (jt/local-date-time 2021 04 20 20 00 00)
+    :end   (jt/local-date-time 2021 04 20 20 10 01)}
+   {:start (jt/local-date-time 2021 04 20 20 10 00)
+    :end   (jt/local-date-time 2021 04 20 20 20 01)}
+   {:start (jt/local-date-time 2021 04 20 20 20 00)
+    :end   (jt/local-date-time 2021 04 20 20 30 01)}
+   ])
+
+(defn add-new-item [sorted-thing log-item]
+  (conj sorted-thing (:end log-item)))
+
+(defn count-overlaps [sorted-thing log-item]
+  (let [start-date (:start log-item)]
+    (count (subseq sorted-thing >= start-date))))
+
+(defn add-new-item-map [s-map log-item]
+  (update s-map (:end log-item) (fn [v] (inc (or v 0)))))
+
+(defn count-overlaps-map [s-map log-item]
+  (let [start-date (:start log-item)]
+    (count (subseq s-map >= start-date))))
+
 (comment
+  (def ss (apply sorted-set (map :end test-data-two-overlaps-1)))
+  (def sm (sorted-map))
+  
+  (for [td test-data-two-overlaps-1]
+    (add-new-item-map sm td))
+  
+  (def new-item {:start (jt/local-date-time 2021 04 20 20 20 00)
+                 :end (jt/local-date-time 2021 04 20 20 21 01)})
+  (count-overlaps ss new-item)
+  (def ss (add-new-item ss new-item))
+  (def sm (add-new-item-map sm new-item))
+  (reduce + (subseq sm >= (:start new-item)))
+  
   ;; java-time fun
   (jt/local-time 10)
   (jt/offset-date-time 2021 10)
@@ -22,17 +67,6 @@
 
   (take 10 (get-logs))
   (map :end (take 10 (get-logs)))
-
-  (def sm (sorted-map 1 :old-one 8 :b 2 :c 3 :d 4 :e 5 :f 1 :one))
-  sm
-  (subseq sm >= 5)
-
-  (def sorted-end-times (sorted-map))
-  (def logs (take 40 (get-logs)))
-  logs
-  (defn inc-end-time [map end-time]
-    (update map end-time (fn [v] (inc (or v 0))))
-    )
-  (reduce inc-end-time sorted-end-times (map :end logs))
+  
    
   )
